4860c6b93d489f7f744889bd14ee41bb4326f7c8
cse-f <verify.moneymind@gmail.com>
2025-10-14 23:41:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -31,5 +31,48 @@ curl -k https://cse-f.github.io/vp/10.js
 11. Write a program to C# to find the smallest digit for a given value.
 curl -k https://cse-f.github.io/vp/11.js
 
+12. Develop the ASP.NET web application using the master page concept to display college information
+curl -k https://cse-f.github.io/vp/12.js
+
+13. Demonstrate how to create windows form that can use HTTP to download and save a resource from a specified URL.
+curl -k https://cse-f.github.io/vp/13.js
+  
+14. Design an ASP.NET Web form to create the registration page by using necessary controls.
+  
+15. Develop the c# console application to create the private assembly and use it in different application.
+curl -k https://cse-f.github.io/vp/15.js
+
+16. Implement a C# console application that checks whether the entered email is in proper format or not using regular expressions.
+curl -k https://cse-f.github.io/vp/16.js
+
+17. Demonstrate the interface inheritance using explicit interface Implementation.
+curl -k https://cse-f.github.io/vp/17.js
+
+18. write a c# console application to demonstrate the delegates concept
+curl -k https://cse-f.github.io/vp/18.js
+
+19. Create ASP.NET Website Application for Employee Management System with a master page which is linked to other web pages in the application
+
+20. Demonstrate how to use LINQ to query data from a database using ADO.NET.
+  
+21 .Problem Definition:
+Create a base class Employee with the following data members:
+name
+employeeID
+salary
+Derive two classes from Employee:
+FullTimeEmployee
+PartTimeEmployee
+Each derived class should implement a method CalculateSalary() as follows:
+FullTimeEmployee Salary Formula:
+salary = (basicSalary * numberOfDays) + HRA + DA
+PartTimeEmployee Salary Formula:
+salary = dailyWages * numberOfDays
+Display the employee details along with the calculated salary
+curl -k https://cse-f.github.io/vp/121.js
+
+22. Develop ASP.NET web form to demonstrate how to use the SmtpMail class to send the mail in the .Net Framework
+
+23. Demonstrate the procedure to validate an email address and phone number in ASP.NET page
 
   
