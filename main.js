1903ee77305b04b7c20f2d80440e58bbfe56f983
cse-f <verify.moneymind@gmail.com>
2025-10-14 19:24:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
 1. Write a C# program to check if a number is prime.
 curl -k https://cse-f.github.io/vp/1.js
+
+2. Write a C# program to print the Fibonacci series up to n terms.
+curl -k https://cse-f.github.io/vp/2.js
+
+3. Write a C# program to check whether a string is a palindrome
+curl -k https://cse-f.github.io/vp/3.js
+
+4. Write a C# program to reverse a number.
+curl -k https://cse-f.github.io/vp/4.js
+
+5. Write a C# program to find the sum of digits of a number
+curl -k https://cse-f.github.io/vp/5.js
+
+6. Write a C# program to count the number of vowels in a string.
+curl -k https://cse-f.github.io/vp/6.js
+
+7. Demonstrate some basic string manipulation using methods of both StringBuilder and String classes.
+curl -k https://cse-f.github.io/vp/7.js
+
+8. Write a C# program to split a substring from a given string.
+curl -k https://cse-f.github.io/vp/8.js
+
+9. C# program to count the odd numbers in the given number
+curl -k https://cse-f.github.io/vp/9.js
+
+10. C# Write a C# program to find the maximum occurring character in a given string.
+curl -k https://cse-f.github.io/vp/10.js
+
+11. Write a program to C# to find the smallest digit for a given value.
+curl -k https://cse-f.github.io/vp/11.js
+
+
+  
