27b51814de5d00e371ede881472c243e86b358b5
cse-f <verify.moneymind@gmail.com>
2025-10-14 21:30:20 +0530

    Create 17.js

---
+++ 17.js
@@ -0,0 +1,27 @@
+using System;
+
+interface IEmployee
+{
+    void Work();
+}
+interface IManager : IEmployee
+{
+    void Manage();
+}
+class Program:IManager
+{
+    void IManager.Manage()
+    {
+        Console.WriteLine("Managing");
+    }
+    void IEmployee.Work()
+    {
+        Console.WriteLine("works");
+    }
+    static void Main(string[] args)
+    {
+        IManager m = new Program();
+        m.Manage();
+        m.Work();
+    }
+}
